74db965f1ef243163ae2d92366ff057b9517865a
renova213 <rizcorenova31@gmail.com>
2022-11-10 11:14:22 +0700

    added poppins font

--- lib/config/theme/app_font.dart
+++ lib/config/theme/app_font.dart
@@ -1,152 +1,153 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppFont {
-  static TextStyle get hero => TextStyle(
+  static TextStyle get hero => GoogleFonts.poppins(
         fontSize: 72.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get display1 => TextStyle(
+  static TextStyle get display1 => GoogleFonts.poppins(
         fontSize: 56.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get display2 => TextStyle(
+  static TextStyle get display2 => GoogleFonts.poppins(
         fontSize: 40.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get display3 => TextStyle(
+  static TextStyle get display3 => GoogleFonts.poppins(
         fontSize: 32.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get heading1 => TextStyle(
+  static TextStyle get heading1 => GoogleFonts.poppins(
         fontSize: 24.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get heading2 => TextStyle(
+  static TextStyle get heading2 => GoogleFonts.poppins(
         fontSize: 20.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphLarge => TextStyle(
+  static TextStyle get paragraphLarge => GoogleFonts.poppins(
         fontSize: 16.sp,
         fontWeight: FontWeight.w400,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphLargeBold => TextStyle(
+  static TextStyle get paragraphLargeBold => GoogleFonts.poppins(
         fontSize: 16.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphMedium => TextStyle(
+  static TextStyle get paragraphMedium => GoogleFonts.poppins(
         fontSize: 14.sp,
         fontWeight: FontWeight.w400,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphMediumBold => TextStyle(
+  static TextStyle get paragraphMediumBold => GoogleFonts.poppins(
         fontSize: 14.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphSmall => TextStyle(
+  static TextStyle get paragraphSmall => GoogleFonts.poppins(
         fontSize: 12.sp,
         fontWeight: FontWeight.w400,
         color: Colors.black,
       );
 
-  static TextStyle get paragraphSmallBold => TextStyle(
+  static TextStyle get paragraphSmallBold => GoogleFonts.poppins(
         fontSize: 12.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get buttonLarge => TextStyle(
+  static TextStyle get buttonLarge => GoogleFonts.poppins(
         fontSize: 16.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get buttonMedium => TextStyle(
+  static TextStyle get buttonMedium => GoogleFonts.poppins(
         fontSize: 14.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get buttonSmall => TextStyle(
+  static TextStyle get buttonSmall => GoogleFonts.poppins(
         fontSize: 12.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get componentLarge => TextStyle(
+  static TextStyle get componentLarge => GoogleFonts.poppins(
         fontSize: 16.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get componentMediumBold => TextStyle(
+  static TextStyle get componentMediumBold => GoogleFonts.poppins(
         fontSize: 14.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get componentMedium => TextStyle(
+  static TextStyle get componentMedium => GoogleFonts.poppins(
         fontSize: 14.sp,
         fontWeight: FontWeight.w400,
         color: Colors.black,
       );
 
-  static TextStyle get componentSmall => TextStyle(
+  static TextStyle get componentSmall => GoogleFonts.poppins(
         fontSize: 12.sp,
         fontWeight: FontWeight.w600,
         color: Colors.black,
       );
 
-  static TextStyle get h0 => TextStyle(
+  static TextStyle get h0 => GoogleFonts.poppins(
         fontSize: 72.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get h1 => TextStyle(
+  static TextStyle get h1 => GoogleFonts.poppins(
         fontSize: 56.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get h2 => TextStyle(
+  static TextStyle get h2 => GoogleFonts.poppins(
         fontSize: 40.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get h3 => TextStyle(
+  static TextStyle get h3 => GoogleFonts.poppins(
         fontSize: 32.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get h4 => TextStyle(
+  static TextStyle get h4 => GoogleFonts.poppins(
         fontSize: 24.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
       );
 
-  static TextStyle get h5 => TextStyle(
+  static TextStyle get h5 => GoogleFonts.poppins(
         fontSize: 20.sp,
         fontWeight: FontWeight.w700,
         color: Colors.black,
