4264add437ca13b704520bb97d66558d942d5243
renova213 <rizcorenova31@gmail.com>
2022-11-11 09:20:05 +0700

    fix error children provider is empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:e_commerce/utils/responsive/responsive_layout_helper.dart';
 import 'package:e_commerce/views/home/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,19 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: const [],
-      child: MultiBlocProvider(
-        providers: const [],
-        child: ScreenUtilInit(
-          designSize: ResponsiveLayout.isMobile(context)
-              ? const Size(360, 640)
-              : const Size(601, 962),
-          builder: (context, _) => const MaterialApp(
-            home: HomeScreen(),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      builder: (context, _) => const Scaffold(
+        body: Center(
+          child: Text("Home"),
+        ),
+      ),
+    );
   }
 }
